0c079b20457581baae9925444e702833e2fef2d7
Mohamed Elsharkawy <64488126+elsharkawymohamed@users.noreply.github.com>
2021-04-02 22:40:29 +0200

    Update segmentTree.cpp

--- segmentTree.cpp
+++ segmentTree.cpp
@@ -5,19 +5,12 @@ using namespace std;
 
 // Range Sum Query (with lazy propagation)
 
-class segmentTree{  //note arr must be 1 based arr
-public:
-    int N;
+class segmentTree{ //note arr must be 1 based arr
+    private:
+     int N;
     vector<int>ar,segT,lazy;
-
-    segmentTree(vector<int>&arr){ // size of array must be a power of 2 + 1 (one-based)
-        N =(int) arr.size()-1;
-        ar = arr;
-        lazy.resize(N<<1); // no of nodes is 2N-1 but we add 1 to be 1 index
-        segT.resize(N<<1);
-        build(1,1,N);
-    }
-    void build(int node,int l,int r){ //o (2n)
+    
+     void build(int node,int l,int r){ //o (2n)
         if(l==r){
             segT[node] = ar[l];
             return;
@@ -28,11 +21,7 @@ public:
         build(node<<1|1,mid+1,r);
         segT[node]=segT[node<<1]+segT[node<<1|1];
     }
-
-    int query(int i,int j){
-        return query(1,1,N,i,j);
-    }
-    int query(int node,int l ,int r,int i,int j){  // O(log n)
+     int query(int node,int l ,int r,int i,int j){  // O(log n)
         if(i>r || j <l)
             return 0;
         if(i<=l && j>=r){
@@ -42,8 +31,8 @@ public:
         // propagate(node, l, r);
         return query(node<<1,l,mid,i,j) + query(node<<1|1,mid+1,r,i,j);
     }
-
-    void updatePoint(int idx,int val){  // O(log n)
+    
+     void updatePoint(int idx,int val){  // O(log n)
         int cur=idx+ N-1;
         segT[cur] += val;
         while(cur!=1){
@@ -51,8 +40,8 @@ public:
             segT[cur]=segT[cur<<1]+segT[cur<<1|1];
         }
     }
-
-    void propagate(int node,int l,int r){ // o(1)
+    
+     void propagate(int node,int l,int r){ // o(1)
         int mid = r + l >> 1;
         lazy[node<<1] += lazy[node];
         lazy[node<<1|1] += lazy[node];
@@ -62,11 +51,7 @@ public:
         lazy[node]=0;
     }
 
-    void updateRange(int i,int j,int val){
-        updateRange(1,1,N, i, j , val);
-
-    }
-    void updateRange(int node ,int l,int r,int i,int j,int val) {  //O(log n)
+     void updateRange(int node ,int l,int r,int i,int j,int val) {  //O(log n)
         if(i>r || j <l)
             return ;
         if (i <= l && r <= j) { // if I took the whole node
@@ -82,6 +67,23 @@ public:
         }
 
     }
+
+public:
+    segmentTree(vector<int>&arr){ // size of array must be a power of 2 + 1 (one-based)
+        N =(int) arr.size()-1;
+        ar = arr;
+        lazy.resize(N<<1); // no of nodes is 2N-1 but we add 1 to be 1 index
+        segT.resize(N<<1);
+        build(1,1,N);
+    }
+   
+    int query(int i,int j){
+        return query(1,1,N,i,j);
+    }
+    void updateRange(int i,int j,int val){
+        updateRange(1,1,N, i, j , val);
+    }
+   
 };
 
 
